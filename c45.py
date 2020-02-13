5d44e8483589c149f55cdeb662b6ba2ee5888b2a
Fithratulhay <pribadi191200@gmail.com>
2020-02-13 13:43:48 +0700

    udah digabung sama nanis

--- c45.py
+++ c45.py
@@ -1,6 +1,107 @@
-import numpy as np
 
-def c45ContinousHandling(df, column_name, entropy):
+import pandas as pd
+import numpy as np
+import operator
+
+eps = np.finfo(float).eps
+
+play_tennis = pd.read_csv("play-tennis.csv")
+print(play_tennis)
+print()
+
+
+def globalEntropy(df):
+	entropy = 0
+	vals = df.iloc[:,-1].unique()
+	len_vals = len(df.iloc[:,-1])
+	for val in vals:
+		p = df.iloc[:,-1].value_counts()[val]/len_vals
+		entropy = entropy + -p*safe_log2(p)
+	return entropy
+
+def safe_log2(x):
+    if x <= 0:
+        return 0
+    return np.log2(x)
+
+def attrEntropy(df,attrName):
+	attrs = df.iloc[:,-1].unique()
+	attrVals = df[attrName].unique()
+
+	#print (attrs)
+	#print(attrVals)
+	
+	entropy = 0
+	for attrVal in attrVals:
+		ent = 0
+		for attr in attrs:
+			sv = len(df[attrName][df.iloc[:,-1] == attr][df[attrName] == attrVal])
+			s = len(df[attrName][df[attrName] == attrVal])
+			frac = sv/(s)
+			ent += -frac*safe_log2(frac)
+		t = s/len(df)
+		entropy += -t*ent
+	
+	return (abs(entropy))
+
+def informationGain(rootEntropy, attrEntropy):
+	return rootEntropy - attrEntropy
+
+def bestAttr(df, cols):
+	gains = {}
+	for col in cols:
+		gains[col] = informationGain(globalEntropy(df), attrEntropy(df, col))
+	return max(gains, key=gains.get)
+
+def filterTab(df, attr, val):
+	return (df.loc[df[attr]==val])
+
+def check_all_attr(datas, wished_value):
+	for data in datas:
+		if data != wished_value:
+			return False
+	return True
+
+def id3(df, targetAttr, attrs, tree = None):
+
+	if tree is None:
+		tree = {}
+	
+	if check_all_attr(df[targetAttr], 'Yes'):
+		tree = {'Yes'}
+		return tree
+	if check_all_attr(df[targetAttr], 'No'):
+		tree = {'No'}
+		return tree
+
+	attr = bestAttr(df, attrs)
+	attrs.remove(attr)
+	tree[attr] = {}
+	
+
+	for attrVal in df[attr].unique():
+
+		tree[attr][attrVal] = {}
+		new_df = df.loc[df[attr] == attrVal]
+		tree[attr][attrVal] = id3(new_df, targetAttr, attrs, tree[attr])
+		
+	return tree	
+
+def bestAttrc45(df, cols):
+	gains = {}
+	nonobject = {}
+	for col in cols:
+		if df[col].dtype != 'object' :
+			nonobject[col], gains[col] = c45ContinousHandling(df, col)
+		else :
+			gains[col] = informationGain(globalEntropy(df), attrEntropy(df, col))
+	
+	if max(gains, key=gains.get) in nonobject :
+		df.loc[:,max(gains, key=gains.get)] = nonobject[max(gains, key=gains.get)]
+
+	return max(gains, key=gains.get)
+
+def c45ContinousHandling(df, column_name):
     values = sorted(df[column_name].unique())
 
     if len(values) == 1:
@@ -16,13 +117,46 @@ def c45ContinousHandling(df, column_name, entropy):
 
             subset1_prob = len(subset1) / len(df[column_name])
             subset2_prob = len(subset2) / len(df[column_name])
+            
+            gains[i] = globalEntropy(df) - subset1_prob*globalEntropy(subset1) - subset2_prob*globalEntropy(subset2)
+            
 
-            gains[i] = entropy - subset1_prob*CountEntropy(subset1) - subset2_prob*CountEntropy(subset2)
-        
-        threshold = values[gains.index(max(gains))]
-        df[column_name] = np.where(df[column_name] <= threshold, "<="+str(threshold), ">"+str(threshold))
+    winner_gain = max(gains)
+    threshold = values[gains.index(max(gains))] 
+    temp = np.where(df[column_name] <= threshold, "<="+str(threshold), ">"+str(threshold))
+    
+    return temp, winner_gain
 
-        return df
+def c45(df, targetAttr, attrs, tree = None):
 
-def CountEntropy(subset) :
-    return 0
+	if tree is None:
+		tree = {}
+	
+	if check_all_attr(df[targetAttr], 'Yes'):
+		tree = {'Yes'}
+		return tree
+	if check_all_attr(df[targetAttr], 'No'):
+		tree = {'No'}
+		return tree
+
+	attr = bestAttrc45(df, attrs)
+	attrs.remove(attr)
+	tree[attr] = {}
+	
+
+	for attrVal in df[attr].unique():
+
+		tree[attr][attrVal] = {}
+		new_df = df.loc[df[attr] == attrVal]
+		tree[attr][attrVal] = c45(new_df, targetAttr, attrs, tree[attr])
+		
+	return tree
+
+#print(informationGain(globalEntropy(play_tennis), attrEntropy(play_tennis, 'outlook')))
+#bestAttr(play_tennis)
+#print(bestAttr(play_tennis))
+#print(filterTab(play_tennis, 'outlook', 'Sunny'))
+#print(check_all_attr(play_tennis.loc[play_tennis['outlook'] == 'Overcast'].iloc[:,-1], 'no'))
+
+#print(id3(play_tennis, 'play', list(['outlook', 'temp', 'humidity', 'wind'])))
+print(c45(play_tennis, 'play', list(['outlook', 'temp', 'humidity', 'wind'])))
